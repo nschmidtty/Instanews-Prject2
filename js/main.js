85907a02381d62de6187db1647e31bc5e3721634
Nate Schmidt <nschmidt1@hotmail.com>
2017-05-01 13:29:08 -0700

    Java Script complete mid css update

--- js/main.js
+++ js/main.js
@@ -1,46 +1,44 @@
 $(function () {
 
-  $('select').on('change', function() {
+  $('select').on('change', function () {
 
-  // var topic = ('option').value();
-  //  var url123 = "https://api.nytimes.com/svc/topstories/v2/" + topic + ".json";
-  //  console.log(url123);
+    
 
-  var url = "https://api.nytimes.com/svc/topstories/v2/opinion.json";
+    $('header').css('height', 'auto');
+    $('img').css('height', 'auto');
 
-  url += '?' + $.param({
-  'api-key': "e4e1ecf1196040c68e4166a75b2fffc2"
+    document.getElementById("logo").classList.add('img-clicked');
+    document.getElementById("logo").classList.remove('img-no-clicked');
 
-  });
-  
-  console.log(url);  
+    var topic = $('select').find(':selected').attr('value')
+    var topic_url = 'https://api.nytimes.com/svc/topstories/v2/' + topic + '.json' + '?' 
+                    + $.param({  'api-key': 'e4e1ecf1196040c68e4166a75b2fffc2'
+                    });
+    console.log(topic_url);
 
-  $.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(data) {
-   var news = data.results.filter(function(value){
-   return (value.multimedia !== undefined)
-  });
-   
-  news = news.slice(0, 12);
+    $.ajax({
+        url: topic_url,
+        method: 'GET',
+      }).done(function (data) {
 
-  console.log(news);
+        
 
-  $.each(news, function(index, value){
-    console.log(value.multimedia[1].url);
-    $('.news-stories').append('<a href="' + value.url + '" id="' + index +'">' + value.abstract + '</a>');
-    $( "#"+index ).css( "background-image", 'url('+value.multimedia[4].url)+')';
-  })
+        var news = data.results.filter(function (value) {
+          return (value.multimedia.length)
+        });
 
-  
- 
+        news = news.slice(0, 12);
 
+        $.each(news, function (index, value) {
+          //console.log(value.multimedia[1].url);
+          $('.news-stories').append('<a href="' + value.url + '" id="' + index + '"><p>' + value.abstract + '</p></a>');
+          $('#' + index).css('background-image', 'url(' + value.multimedia[4].url) + ')';
+        })
 
-    })
-  
-  .fail(function(err) {
-    throw err;
+      })
+
+      .fail(function (err) {
+        throw err;
+      });
   });
 });
-}); 
